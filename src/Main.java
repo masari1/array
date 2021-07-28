e73ab0d981b94c25c0636d734c801b8d087d085f
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 13:53:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+
+    public static void main(String [] args){
+
+
+     
+
+        String [] carss = new String [3];
+
+        carss [0] = "camaro";
+        carss [1] = "corvette";
+        carss [2] = "Tesla";
+
+        for (int i=0; i < carss.length; i++){
+            System.out.println(carss[i]);
+        }
+
+
+
+
+    }
+
+}
